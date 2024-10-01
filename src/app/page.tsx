eb35ca41b5aaaf35131edfcf0efdbdbc87aeac80
Abdul Wahab <138891037+wahab-277@users.noreply.github.com>
2024-10-01 10:26:02 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Home = () => {
+
+  return (
+    <body>
+
+
+                <div className="main">
+                             Hi,
+                             <br />
+                             This is <span className="pinkColor"> ABDUL WAHAB</span>
+                             <br /> Enhancing experience with NEXT.JS .|
+                </div>
+                  
+                <div className="main">
+                             <img src="amar.jpg" alt="amar" className="profile"/>
+                </div>
+
+
+
+
+  </body>
+
+
+
+  )
+
+};
+
+export default Home;
